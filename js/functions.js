32b82e9cf4f2f685b528d632c88d95dc2f0267c7
Maciej Zaradkiewicz <mzaradkiewicz@gmail.com>
2020-06-18 19:04:56 +0200

    final version

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,45 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(argMoveId) {
+	console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+	if (argMoveId == 1) {
+	  return 'kamień';
+	} else if (argMoveId == 2) {
+	  return 'papier';
+	} else if (argMoveId == 3) {
+	  return 'nożyce';
+	} else {
+	  printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+	  return 'kamień';
+	}
+  }
+
+  function displayResult(argPlayerMove, argComputerMove) {
+	console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+	if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+	  printMessage('Wygrywasz!');
+	} else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+	  printMessage('Wygrywasz!');
+	} else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+	  printMessage('Wygrywasz!');
+	} else if (argPlayerMove == argComputerMove) {
+	  printMessage('Remis!');
+	} else {
+	  printMessage('Przegrywasz :(');
+	}
+	printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  }
+
+  function buttonClicked(argButtonName) {
+	clearMessages();
+	console.log(argButtonName + ' został kliknięty');
+  }
+
+  function getPlayerMove (playerMove) {
+	randomNumber = Math.floor(Math.random() * 3 + 1);
+	console.log('wylosowana liczba to: ' + randomNumber);
+	computerMove = getMoveName(randomNumber);
+	displayResult(playerMove, computerMove);
+  }
